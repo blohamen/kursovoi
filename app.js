bb70d3d508266ed8bc3d6d63b0e6fda4d7be5a05
Ilya Staver <ilya_staver@epam.com>
2018-11-09 00:13:41 +0300

    Refactoring app.js, add babel

--- app.js
+++ app.js
@@ -1,107 +1,81 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const path = require('path');
- 
-const user  = require('./models/user');
-const problem = require('./models/problem');
+const userModel  = require('./models/user');
+const problemModel = require('./models/problem');
 const app = express();
-app.set("view engine","ejs");
-app.use(bodyParser.urlencoded({extended: true}));
 
+app.set("view engine","ejs");
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+  res.render('index');
+});
 
 app.get('/form', (req, res) => res.render('form'));
 
-
-
-let expert; // идентификационная переменная (вместо cookie)
-let admin;
 app.get('/registration', (req, res) => res.render('registration'));
-app.get('/admin', (req, res) => {
-  if(!admin) res.render('403');
-  user.find({})
-  .then(data=>{
-    res.render('admin', {users: data});
-  })
+
+app.get('/admin', async (req, res) => {
+  const usersData = await userModel.find({});
+  return res.render('admin', { users: usersData });
 });
 
-app.get('/user', (req, res) => {
-      problem.find({})
-      .then(problems =>{
-        user.find({rule:"expert"})
-        .then(users =>{
-          res.render('user', {users:users, problems: problems});
-        })
-      }) 
+app.get('/user', async (req, res) => {
+  const problemsData = await problemModel.find({});
+  const usersData = await userModel.find({ rule: 'expert' });
+  return res.render('user', { users:usersData, problems: problemsData });
 });
 
-app.get('/expert', (req, res) => {
-  if(!expert) res.render('403');
-  problem.find({})
-  .then(problems=>{
-    res.render('expert', {problems: problems, expert:expert});
-  });
+app.get('/expert', async (req, res) => {
+  const problems = await problemModel.find({}); // todo pass expert
+  return res.render('expert', { problems, expert:expert });
 });
 
-
-app.post('/registration', (req,res) =>{
-  const {login, password} = req.body;
-  user.findOne({login: new RegExp('^'+login+'$', "i")}, function(err,User){
-    if(!User) {
-      user.create({
-        login: login,
-        password: password,
-        rule: "user"
-      });
-      res.redirect('/form');
-    }
-    else{
-    res.sendStatus(400);
-    }
-  });
-})
-
-app.post('/form', (req,res) =>{
-  const {login, password} = req.body;
-  user.findOne({
-    login: new RegExp('^'+login+'$', "i"), 
-    password: new RegExp('^'+password+'$', "i")
-  }, function(err, User){
-    if(User && User.rule === "admin"){
-      admin = JSON.stringify(User);
-     res.redirect('/admin');
-  } else if(User && User.rule === "user"){
-    res.redirect('/user');
-  } else if(User && User.rule === "expert"){
-    expert = JSON.stringify(User);
-    res.redirect("/expert");  
-  }
-    else  {
-     res.sendStatus(400);
-    } 
-  })
-});
-
-app.post('/expert', bodyParser.json(), function(req, res){
-    user.findOneAndUpdate(JSON.parse(expert), req.body, function(err){
-      if(err) res.sendStatus(500);
-      return res.sendStatus(200);
+app.post('/registration', async (req,res) => {
+  const { login, password } = req.body;
+  const user = await userModel.findOne({login: new RegExp('^'+login+'$', "i")});
+  if (!user) {
+    userModel.create({
+      login: login,
+      password: password,
+      rule: "user"
     });
-})
+    return res.redirect('/form');
+  }
+  return res.sendStatus(400);
+});
 
-app.get('/', function (req, res) {
-  res.render('index');
-})
- 
+app.post('/form', async (req,res) => {
+  const { login, password } = req.body;
+  const user = await userModel.findOne({
+    login: new RegExp('^'+login+'$', "i"),
+    password: new RegExp('^'+password+'$', "i")
+  });
+    if(!user) {
+       return res.sendStatus(400);
+    }
+  switch (user.rule) {
+    case 'admin': return res.redirect('/admin');
+    case 'user': return res.redirect('/user');
+    case 'expert': return res.redirect("/expert");
+  }
+});
+
+app.post('/expert', bodyParser.json(), async (req, res) => {
+  await userModel.findOneAndUpdate(JSON.parse(expert), req.body);
+  return res.sendStatus(200);
+});
+
 app.post('/admin', bodyParser.json(), function(req, res){
-  user.findOneAndRemove(req.body, function(err){
+  userModel.findOneAndRemove(req.body, function(err) {
     if(err) res.sendStatus(500);
   });
 })
 
-app.use(function(req, res,next) {
-  res.status(404).render("404");
-});
+// app.use(function(req, res) {
+//   res.status(404).render("404");
+// });
 
 module.exports = app;  
